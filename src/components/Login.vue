ce4501185379af404b4f1ef0cc4d286fccb8da6d
lzx_ <2674920784@qq.com>
2020-02-29 00:25:27 +0800

    redirect route to login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    Login compo
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
 </script>
 
-<style>
+<style  scoped>
 
 </style>
