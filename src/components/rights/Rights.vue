23c67278351897dddcefded465c8bdb1cf0ddd0f
lzx_ <2674920784@qq.com>
2020-03-10 11:22:38 +0800

    column mulitple check filter

--- src/components/rights/Rights.vue
+++ src/components/rights/Rights.vue
@@ -13,11 +13,17 @@
         <el-table-column type="index" label="#"></el-table-column>
         <el-table-column label="Access Item" prop="authName"></el-table-column>
         <el-table-column label="Path" prop="path"></el-table-column>
-        <el-table-column label="Access Level" prop="level">
-          <template v-slot="slotLevel">
-            <el-tag v-if="slotLevel.row.level === '0'">Level-1</el-tag>
-            <el-tag type="success" v-else-if="slotLevel.row.level === '1'">Level-2</el-tag>
-            <el-tag type="warning" v-else-if="slotLevel.row.level === '2'">Level-3</el-tag>
+        <el-table-column label="Access Level"
+        prop="level"
+        sortable
+        column-key="level"
+        :filters="[{ text: 'Level-1', value: '0' }, { text: 'Level-2', value: '1' }, { text: 'Level-3', value: '3' }]"
+        :filter-method="filterTag"
+        >
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level === '0'">Level-1</el-tag>
+            <el-tag type="success" v-else-if="scope.row.level === '1'">Level-2</el-tag>
+            <el-tag type="warning" v-else-if="scope.row.level === '2'">Level-3</el-tag>
           </template>
         </el-table-column>
       </el-table>
@@ -42,9 +48,14 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error('Failed to load access list')
       } else {
-        console.log(res)
+        // console.log(res)
         this.rightsList = res.data
       }
+    },
+    filterTag (value, row, column) {
+      // console.log(row.level)
+      const property = column.property
+      return row[property] === value
     }
   }
 }
